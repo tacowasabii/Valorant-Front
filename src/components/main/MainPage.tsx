6b9625e949086b5648f8e750b7a8c552db669c33
tacowasabii <godseoril812@gmail.com>
2023-05-16 18:02:33 +0900

    supabase

--- src/components/main/MainPage.tsx
+++ src/components/main/MainPage.tsx
@@ -1,7 +1,36 @@
+import { createClient } from "@supabase/supabase-js";
 import { svgRiot, svgSearch } from "@styles/svg";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
+type playerRank = {
+  id: number;
+  rank: number;
+  player: string;
+  rating: number;
+  win_rate: string;
+};
 
 const MainPage = () => {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+  );
+
+  console.log(supabase);
+  const [players, setPlayers] = useState<playerRank[]>([]);
+
+  useEffect(() => {
+    const fetchPlayers = async () => {
+      const { data, error } = await supabase?.from("kr_rank").select("*");
+      if (error) console.error("Data fetch error:", error);
+      else setPlayers(data as playerRank[]);
+    };
+    fetchPlayers();
+  }, []);
+
+  console.log(players);
+
   return (
     <div className="bg-slate-900">
       <div className="xl:min-h-[430px] min-h-[500px] bg-valorant flex flex-col items-center bg-center bg-cover">
@@ -34,204 +63,30 @@ const MainPage = () => {
           <div className="w-32 flex justify-center items-center">레이팅</div>
           <div className="w-28 flex justify-center items-center">승률</div>
         </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">1</div>
-          <div className="w-80 flex justify-center items-center">
-            Ебать ты лот#0000
+        {players.map((player) => (
+          <div key={player.id} className="flex text-gray-200 h-12 bg-slate-800">
+            <div className="w-24 flex justify-center items-center">
+              {player.rank}
+            </div>
+            <div className="w-80 flex justify-center items-center">
+              {player.player}
+            </div>
+            <div className="w-32 flex justify-center items-center">
+              <Image
+                className="mr-1"
+                src="/rank.png"
+                alt="레디언트"
+                width="24"
+                height="24"
+                loading="lazy"
+              />
+              {player.rating}
+            </div>
+            <div className="w-28 flex justify-center items-center text-blue-500">
+              {player.win_rate}
+            </div>
           </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            635
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            71.2%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">2</div>
-          <div className="w-80 flex justify-center items-center">
-            Спасибо#0001
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            509
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            76.0%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">3</div>
-          <div className="w-80 flex justify-center items-center">KIT#LFT</div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            471
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            74.1%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">4</div>
-          <div className="w-80 flex justify-center items-center">
-            게임에재능이없는사람#0630
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            468
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            64.3%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">5</div>
-          <div className="w-80 flex justify-center items-center">
-            EverydaY#eProd
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            458
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            69.6%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">6</div>
-          <div className="w-80 flex justify-center items-center">
-            Γειά σου#0001
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            446
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            66.7%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">7</div>
-          <div className="w-80 flex justify-center items-center">
-            TOXIC Distic#yet
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            390
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            66.7%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">8</div>
-          <div className="w-80 flex justify-center items-center">
-            김플론#0808
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            388
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            62.2%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">9</div>
-          <div className="w-80 flex justify-center items-center">
-            Secret Agent
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            388
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            63.0%
-          </div>
-        </div>
-        <div className="flex text-gray-200 h-12 bg-slate-800">
-          <div className="w-24 flex justify-center items-center">10</div>
-          <div className="w-80 flex justify-center items-center">
-            MWD PERES#IWNL
-          </div>
-          <div className="w-32 flex justify-center items-center">
-            <Image
-              className="mr-1"
-              src="/rank.png"
-              alt="레디언트"
-              width="24"
-              height="24"
-              loading="lazy"
-            />
-            388
-          </div>
-          <div className="w-28 flex justify-center items-center text-blue-500">
-            67.3%
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
