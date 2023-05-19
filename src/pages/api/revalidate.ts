bc1e8f39b69498efee72383e7ef8838e50934746
tacowasabii <godseoril812@gmail.com>
2023-05-19 16:38:12 +0900

    isr

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,14 +4,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  {
-    if (req.query.secret !== process.env.SECRET_REVALIDATE_TOKEN) {
-      return res.status(401).json({ message: "Invalid token" });
-    }
-    const path = req.query.path as string;
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.SECRET_REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
 
-    await res.revalidate(path);
-
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate("/isr");
     return res.json({ revalidated: true });
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send("Error revalidating");
   }
 }
--- src/pages/isr.tsx
+++ src/pages/isr.tsx
@@ -1,10 +1,9 @@
-import { ReactElement, useEffect } from "react";
+import { ReactElement } from "react";
 import { Layout } from "@components/common";
 import { GetStaticProps } from "next";
 import supabase from "@utils/supabase";
 import { IsrPage } from "@components/main";
 import { NextPageWithLayoutProps } from "types/base";
-import axios from "axios";
 
 export const getStaticProps: GetStaticProps<playerProps> = async () => {
   const { data: kr_rank, error } = await supabase.from("kr_rank").select("*");
@@ -24,21 +23,6 @@ export const getStaticProps: GetStaticProps<playerProps> = async () => {
 const Isr: NextPageWithLayoutProps<playerProps> & {
   getLayout?: (page: ReactElement) => ReactElement;
 } = ({ players }: playerProps) => {
-  useEffect(() => {
-    const revalidateData = async () => {
-      const secret =
-        "21a049423c76c89ebf547887819046d02ab9ec6e58fc9e6e5fe5c7dc900a44014dce9f07ae0a33ba80fca5dab4941914c01b21212469661b6a077b9b065a536a";
-
-      try {
-        await axios.post(`/api/revalidate?secret=${secret}`);
-      } catch (error) {
-        console.error("Revalidation error:", error);
-      }
-    };
-
-    revalidateData();
-  }, []);
-
   return <IsrPage players={players} />;
 };
 
