c6b4c601f9269c5ba4518f6a34bffbb73f323ee5
tacowasabii <godseoril812@gmail.com>
2023-05-22 16:05:07 +0900

    on-demand revalidation

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,7 +4,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { method, query, body } = req;
+  const { method, query } = req;
 
   if (method !== "POST") {
     return res
@@ -12,23 +12,13 @@ export default async function handler(
       .json({ error: "Invalid HTTP method. Only POST method is allowed." });
   }
 
-  // Unauthorized access as invalid token
   if (query.secret !== process.env.SECRET_REVALIDATE_TOKEN) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
   try {
-    if (!body) {
-      res.status(400).send("Bad reqeust (no body)");
-      return;
-    }
-
-    const idToRevalidate = body.id;
-
-    if (idToRevalidate) {
-      await res.revalidate("/isr");
-      return res.json({ revalidated: true });
-    }
+    await res.revalidate("/isr");
+    return res.json({ revalidated: true });
   } catch (err) {
     return res.status(500).send("Error while revalidating");
   }
